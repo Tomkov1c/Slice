5d7fb5c5047c4a9ef6ea93f1151be0d5ca50b2ff
Tomkov1c <tomkliner9@gmail.com>
2026-01-15 14:31:22 +0100

    Make TexturePackCustomValues parse data from json

--- common/src/main/java/com/tomkovic/slice/classes/TexturePackCustomValues.java
+++ common/src/main/java/com/tomkovic/slice/classes/TexturePackCustomValues.java
@@ -1,93 +1,106 @@
 package com.tomkovic.slice.classes;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.google.gson.annotations.SerializedName;
+
+import com.tomkovic.slice.helpers.JsonHelper;
+
 import net.minecraft.client.Minecraft;
-import net.minecraft.server.packs.resources.Resource;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.ResourceManager;
+
 
 public class TexturePackCustomValues {
+
     @SerializedName("offset_from_center")
-    public static boolean offsetFromCenter;
+    public boolean offsetFromCenter;
     
     @SerializedName("x_offset")
-    public static int xOffset;
+    public int xOffset;
     
     @SerializedName("y_offset")
-    public static int yOffset;
+    public int yOffset;
     
     @SerializedName("x_offset_hovered")
-    public static int xOffsetHovered;
+    public int xOffsetHovered;
     
     @SerializedName("y_offset_hovered")
-    public static int yOffsetHovered;
+    public int yOffsetHovered;
     
     @SerializedName("x_offset_active")
-    public static int xOffsetActive;
+    public int xOffsetActive;
     
     @SerializedName("y_offset_active")
-    public static int yOffsetActive;
+    public int yOffsetActive;
     
     @SerializedName("item_x_offset")
-    public static int itemXOffset;
+    public int itemXOffset;
     
     @SerializedName("item_y_offset")
-    public static int itemYOffset;
+    public int itemYOffset;
     
     @SerializedName("item_x_offset_hovered")
-    public static int itemXOffsetHovered;
+    public int itemXOffsetHovered;
     
     @SerializedName("item_y_offset_hovered")
-    public static int itemYOffsetHovered;
+    public int itemYOffsetHovered;
     
     @SerializedName("item_x_offset_active")
-    public static int itemXOffsetActive;
+    public int itemXOffsetActive;
     
     @SerializedName("item_y_offset_active")
-    public static int itemYOffsetActive;
+    public int itemYOffsetActive;
     
     @SerializedName("slot_number_x_offset")
-    public static int slotNumberXOffset;
+    public int slotNumberXOffset;
     
     @SerializedName("slot_number_y_offset")
-    public static int slotNumberYOffset;
+    public int slotNumberYOffset;
     
     @SerializedName("slot_number_x_offset_hovered")
-    public static int slotNumberXOffsetHovered;
+    public int slotNumberXOffsetHovered;
     
     @SerializedName("slot_number_y_offset_hovered")
-    public static int slotNumberYOffsetHovered;
+    public int slotNumberYOffsetHovered;
     
     @SerializedName("slot_number_x_offset_active")
-    public static int slotNumberXOffsetActive;
+    public int slotNumberXOffsetActive;
     
     @SerializedName("slot_number_y_offset_active")
-    public static int slotNumberYOffsetActive;
+    public int slotNumberYOffsetActive;
     
     @SerializedName("slot_number_color")
-    public static String slotNumberColor;
+    public String slotNumberColor;
     
     @SerializedName("slot_number_color_hovered")
-    public static String slotNumberColorHovered;
+    public String slotNumberColorHovered;
     
     @SerializedName("slot_number_color_active")
-    public static String slotNumberColorActive;
+    public String slotNumberColorActive;
     
-    @SerializedName("background_darkening_color")
-    public static String backgroundOverlayColor;
-    
-    @SuppressWarnings({ "static-access", "null" })
-    public static void parseFromResource(ResourceLocation location) throws Exception {
-        Minecraft mc = Minecraft.getInstance();
-        Resource resource = mc.getResourceManager().getResource(location).orElseThrow();
-        
-        try (InputStream stream = resource.open();
-             InputStreamReader reader = new InputStreamReader(stream)) {
+    @SerializedName("background_overlay_color")
+    public String backgroundOverlayColor;
+
+
+    @SuppressWarnings("null")
+    public void parseFromResource(String path) {
+        try {
+            Minecraft mc = Minecraft.getInstance();
+            if (mc == null) return;
+
+            ResourceManager resourceManager = mc.getResourceManager();
+            if (resourceManager == null) return;
+
+            JsonObject json = JsonHelper.readJsonFromResources(resourceManager, path);
+            if (json == null) {
+                System.out.println("Warning: JSON not found at " + path);
+                return;
+            }
+
             Gson gson = new Gson();
-            TexturePackCustomValues temp = gson.fromJson(reader, TexturePackCustomValues.class);
-            
+            TexturePackCustomValues temp = gson.fromJson(json, TexturePackCustomValues.class);
+
+
             offsetFromCenter = temp.offsetFromCenter;
             xOffset = temp.xOffset;
             yOffset = temp.yOffset;
@@ -95,22 +108,30 @@ public class TexturePackCustomValues {
             yOffsetHovered = temp.yOffsetHovered;
             xOffsetActive = temp.xOffsetActive;
             yOffsetActive = temp.yOffsetActive;
+
             itemXOffset = temp.itemXOffset;
             itemYOffset = temp.itemYOffset;
             itemXOffsetHovered = temp.itemXOffsetHovered;
             itemYOffsetHovered = temp.itemYOffsetHovered;
             itemXOffsetActive = temp.itemXOffsetActive;
             itemYOffsetActive = temp.itemYOffsetActive;
+
             slotNumberXOffset = temp.slotNumberXOffset;
             slotNumberYOffset = temp.slotNumberYOffset;
             slotNumberXOffsetHovered = temp.slotNumberXOffsetHovered;
             slotNumberYOffsetHovered = temp.slotNumberYOffsetHovered;
             slotNumberXOffsetActive = temp.slotNumberXOffsetActive;
             slotNumberYOffsetActive = temp.slotNumberYOffsetActive;
+
             slotNumberColor = temp.slotNumberColor;
             slotNumberColorHovered = temp.slotNumberColorHovered;
             slotNumberColorActive = temp.slotNumberColorActive;
             backgroundOverlayColor = temp.backgroundOverlayColor;
+
+        } catch (Exception e) {
+            System.err.println("Error parsing texture pack values from: " + path);
+            e.printStackTrace();
         }
     }
+
 }
