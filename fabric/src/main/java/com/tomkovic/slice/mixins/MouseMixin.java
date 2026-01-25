51a4bed07c6f687a11887360f9f0a1a88d2344af
Tomkov1c <tomkliner9@gmail.com>
2026-01-25 01:18:34 +0100

    Implement DISABLE_HOTBAR_SCROLLING

--- fabric/src/main/java/com/tomkovic/slice/mixins/MouseMixin.java
+++ fabric/src/main/java/com/tomkovic/slice/mixins/MouseMixin.java
@@ -5,6 +5,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import com.tomkovic.slice.GlobalConfig;
 import com.tomkovic.slice.handlers.RadialMenuHandler;
 
 import net.minecraft.client.MouseHandler;
@@ -15,6 +16,11 @@ public class MouseMixin {
     @Inject(method = "grabMouse", at = @At("HEAD"), cancellable = true)
     private void preventMouseGrab(CallbackInfo ci) {
         if (RadialMenuHandler.isMenuOpen) ci.cancel();
+    }
+
+    @Inject(method = "onScroll", at = @At("HEAD"), cancellable = true)
+    private void preventScroll(long window, double horizontal, double vertical, CallbackInfo ci) {
+    	if (GlobalConfig.DISABLE_HOTBAR_SCROLLING) ci.cancel();
     }
 
 }
