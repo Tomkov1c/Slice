03110360fcc37f1522077cd6a626ba3af22c5590
Tomkov1c <tomkliner9@gmail.com>
2026-07-20 13:39:39 +0200

    Remove useless imports

--- common/src/main/java/com/ansi/slice/handlers/RadialMenuHandler.java
+++ common/src/main/java/com/ansi/slice/handlers/RadialMenuHandler.java
@@ -6,7 +6,6 @@ import com.ansi.slice.Constants;
 
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 
 public class RadialMenuHandler {
